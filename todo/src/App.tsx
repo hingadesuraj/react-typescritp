0f03edfa91d26267db25ec61fd279b3ac8c1bba1
Suraj <hingadesuraj@gmail.com>
2024-03-14 17:36:29 +0530

    explore object type

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -2,11 +2,23 @@ import "./App.css";
 
 function App() {
   // basic type in typescript
-  let name : string;
-  let age : number;
-  let isTrue:boolean;
-  let role : [number,string]
-  let students:string[]
+  // let name : string;
+  // let age : number;
+  // let isTrue:boolean;
+  // let role : [number,string]
+  // let students:string[]
+
+  // object type
+  type Person = {
+    name:string,
+    age?:number  // age property is optationl due to question mark
+  }
+
+
+  let person : Person{
+    name:"Suraj",
+    age:21
+  }
 
 
   return (
