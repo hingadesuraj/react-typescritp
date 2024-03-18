1b012712f7330861cfb7029e09d343756a5dd377
Suraj <hingadesuraj@gmail.com>
2024-03-18 18:09:29 +0530

    Todo Component desing complete

--- todo/src/components/TodoList.tsx
+++ todo/src/components/TodoList.tsx
@@ -7,27 +7,40 @@ interface Props {
 
 const TodoList: React.FC<Props> = ({ todos, setTodos }: Props) => {
   return (
-    <div>
-      <div
+    <div >
+     {todos.map((data)=>{
+      return(
+        <div key={data.id}
         style={{
           border: "1px solid black",
           height: "20px",
           width: "500px",
           backgroundColor: "white",
           padding: "5px",
+          marginBottom:"5px"
         }}
       >
-        <div style={{display:"flex",justifyContent:"space-between",alignItems:"center", alignContent:"center"}} >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            alignContent: "center",
+        
+          }}
+        >
           <div>
-            <h5>Todo One </h5>
+            <h5>{data.todo}</h5>
           </div>
-          <div style={{display:"flex",gap:"5px"}} >
-            <button>Edit</button>
-            <button>Delete</button>
-            <button>Complete</button> 
+          <div style={{ display: "flex", gap: "5px"}}>
+            <button style={{cursor:"pointer"}} >Edit</button>
+            <button style={{cursor:"pointer"}} >Delete</button>
+            <button style={{cursor:"pointer"}} >Complete</button>
           </div>
         </div>
       </div>
+      )
+     })}
     </div>
   );
 };
